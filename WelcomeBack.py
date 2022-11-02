1e1daef4901308ebf0bcade9d8d34d68a20aa064
Chidera99 <cnc2688@g.rit.edu>
2022-11-02 00:04:17 -0400

    second commit ayyee

--- WelcomeBack.py
+++ WelcomeBack.py
@@ -3,9 +3,19 @@
 def Chidera(int):
     print("I love my life", int)
 
+def show_me_what_u_got(string):
+    string1="go girl!"
+    count=0
+    while (count<5):
+        count=+1
+        return string+string1
+
+    
+
 
 def main():
     Chidera(5)
+    print(show_me_what_u_got("a coding babe ;), "))
 
 main()
 
